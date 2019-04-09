9ea402693281871fa090c1fbce8836fceb436f68
Jiajie-Mai <jmai3@stuy.edu>
2019-04-09 12:34:58 -0400

    did things

--- HTMLthing/index.js
+++ HTMLthing/index.js
@@ -1,18 +1,37 @@
+// This is a thing.
+// Heck.
 
+var storyDiv = document.getElementById('storyDiv');
+var choices = document.getElementById('choices');
 
-var storyDiv = document.getElementById("storyDiv");
-var choices = document.getElementById("choices");
-
-function addButtons() {
-  var newButton = document.createElement("input");
-  newButton.setAttribute("type", "button");
-  newButton.setAttribute("value", "Start");
-  newButton.setAttribute("name", "start");
-  document.body.appendChild(newButton);
+function addButtons(name, id) {
+  var newButton = document.createElement('input');
+  newButton.setAttribute('type', 'button');
+  newButton.setAttribute('value', name);
+  newButton.id = id;
+  choices.appendChild(newButton);
 }
 
-addButtons()
+function clearDiv(input) {
+  input.innerHTML = "";
+}
 
-Start.addEventListener("click", function(e){
-  storyDiv.innerHTML = "No.";
+function changeHTML(input, HTMLtext) {
+  input.innerHTML += HTMLtext;
+}
+
+addButtons("Start",'start');
+
+start.addEventListener('click', function(e){
+  begin();
 })
+
+function begin(){
+  storyDiv.textContent= "You awake sitting in a chair and a splitting head ache. There is food on the table.";
+  clearDiv(choices);
+  var newDiv = ('div')
+  changeHTML(newDiv, '<input type="text" name="Cry" value="Cry" id="choice1"/>\
+                      <input type="text" name="Consume" value="Consume" id="choice2"/>\
+                      <input type="text" value="Fight" value="Fight" id="choice3"/>'
+                    );
+}
